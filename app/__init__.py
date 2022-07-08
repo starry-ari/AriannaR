b00747cd05a404381797144559da3185beb4a8a6
Arianna <44093846+starry-ari@users.noreply.github.com>
2022-07-08 16:21:26 -0400

    fixed

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,62 @@
+from crypt import methods
 import os
+
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from playhouse.shortcuts import model_to_dict
+from peewee import *
+import datetime
 
 load_dotenv()
 app = Flask(__name__)
 
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        host=os.getenv("MYSQL_HOST"),
+        port=3306
+)
+print(mydb)
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = mydb
+mydb.connect()
+mydb.create_tables([TimelinePost])
 
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), users=users)
 
-@app.route('/haileyHobbies')
-def haileyHobbies():
-    return render_template('haileyHobbies.html', url=os.getenv("URL"), user=Hailey)
 
 @app.route('/ariaHobbies')
 def ariaHobbies():
     return render_template('ariaHobbies.html', url=os.getenv("URL"), user=Aria)
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+
+    return model_to_dict(timeline_post)
+    
+@app.route('/api/timeline_post', methods=['GET'])
+def get_timr_line_post():
+    return { 
+        'timeline_posts': [
+        model_to_dict(p)
+        for p in
+TimelinePost.select().order_by(TimelinePost.created_at.desc())]
+}
+@app.route('/timeline')
+def timeline():
+    return render_template('timeline.html', title="Timeline")
 
 # This is the User class that defines everything that will be inputted into the portfolio template
 class User:
