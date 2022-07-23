b8f8c11b710b58b6acc83b9129d3201ecbe5e539
Arianna <44093846+starry-ari@users.noreply.github.com>
2022-07-22 22:07:19 -0400

    redeploy

---
+++ redeploy-script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+                                                                                     .6/site-packages (from -
+tmux kill-server
+                                                                                     /site-packages (from -r 
+cd AriannaR
+                                                                                     /site-packages (from -r 
+git fetch && git reset origin/main --hard
+                                                                                     site-packages (from -r r
+source venv/bin/activate
+                                                                                     hon3.6/site-packages (fr
+pip install -r  requirements.txt
+                                                                                     /site-packages (from -r 
+tmux new -ds portfolio 'python -m venv python3-virtualvenv;
+source python3-virtualvenv/bin/activate;                                             3.6/site-packages (from 
+pip install -r requirements.txt;
+flask run --host=0.0.0.0;'                                                           e-packages (from Werkzeu
+
+                                                                                     packages (from importlib
+
+                  
