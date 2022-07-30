47d68364943e221b6950177d62eb147ced1d9964
Arianna <44093846+starry-ari@users.noreply.github.com>
2022-07-30 17:15:16 -0400

    new redeploy

--- redeploy-script.sh
+++ redeploy-script.sh
@@ -6,11 +6,6 @@ cd AriannaR
 
 git fetch && git reset origin/main --hard
 
-source python3-virtualvenv/bin/activate 
-
-pip install -r requirements.txt
-
-systemctl restart myportfolio
-                                                            packages (from importlib
-
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
                   
